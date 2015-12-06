6738ab95c04cf7bca917b46ff08094e2a7c5ea5f
Rob Michaels <mykrobb2020@gmail.com>
2015-12-06 11:27:30 -0500

    Second commit

--- app/src/main/java/com/mykrobb2020/presshere/activities/MainActivity.java
+++ app/src/main/java/com/mykrobb2020/presshere/activities/MainActivity.java
@@ -68,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //some changes here
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
